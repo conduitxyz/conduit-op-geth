b92dae476b1580a24a169dda150e9495a04dc696
Andrew Huang <k.andrew.huang@gmail.com>
2023-09-20 11:08:52 -0700

    Add EIP-1153 support and bump maxContractSize and tx validation

--- eth/backend.go
+++ eth/backend.go
@@ -206,6 +206,7 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	var (
 		vmConfig = vm.Config{
 			EnablePreimageRecording: config.EnablePreimageRecording,
+			ExtraEips:               []int{1153},
 		}
 		cacheConfig = &core.CacheConfig{
 			TrieCleanLimit:      config.TrieCleanCache,
